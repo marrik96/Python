404635fe7b1aec3e227b0959fd015b0f05f237dd
marrik96 <ricardo@datawan.net>
2018-11-21 19:45:08 -0600

    Initial commit

---
+++ hello.py
@@ -0,0 +1,10 @@
+# This program says hello and asks for name
+print ('Hello world!')
+print ('What is your name?')
+myName = input()
+print ('It is good to meet you, ' + myName)
+print ('the length of your name is:')
+print (len(myName))
+print ('what is your age?')
+myAge = input()
+print ('you will be ' + str(int(myAge)+1) + ' in a year.')
