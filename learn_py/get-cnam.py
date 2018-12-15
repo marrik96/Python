b9f11362e5adca4c4f09950be87a89a90b3ae867
marrik96 <ricardo@datawan.net>
2018-12-15 14:41:01 -0600

    updates

--- learn_py/get-cnam.py
+++ learn_py/get-cnam.py
@@ -1,13 +1,17 @@
 import requests
 
 cnamUrl = 'https://api.opencnam.com/v3/phone/'
-myDid = input('Enter DID to check')
-myAcct = input('Enter CNAM Account')
-myToken = input('Enter Auth')
-
+myDid = input('Enter DID to check: ')
+if len(myDid) != 11 and myDid.isdigit():
+    print('Please enter valid 11 digit Phone Number')
+myAcct = input('Enter CNAM Account: ')
+myToken = input('Enter Auth: ')
 myResp = requests.get(cnamUrl + myDid + '?account_sid=' + myAcct + '&auth_token=' + myToken)
 if myResp.status_code != 200:
     # Error
-    raise ApiError('GET /phone/ {}'.format(myResp.status_code))
-print(myResp.status_code)
-print(myResp.content)
+    print('HTTP Error: ' + str(myResp.status_code))
+    exit()
+code = myResp.status_code
+name = myResp.content
+response = str(code) + '\n' + str(name)
+print(response.replace('b', ''))
